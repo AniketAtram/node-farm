1e4862f959e54d4b700784e92d6a5a753cc25f7c
AniketAtram <anniketatram@gmail.com>
2024-12-06 14:40:28 +0530

    populated overview page with data

--- index.js
+++ index.js
@@ -5,16 +5,49 @@ const fs = require('fs');
 const http = require('http');
 
 data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+
+}
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/api/data'){
-    res.writeHead(200,{
-      "Content-Type": "application/json"
-    });
-    res.end(data);
+  if (req.url === "/" || req.url === "/overview") {
+    const cardHtml  = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const overviewHtml = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardHtml);
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    })
+    res.end(overviewHtml);
   }
-  else{
-    res.end("Hello from basic http server");
+  else if (req.url === "/product") {
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    })
+    res.end(tempProduct)
+  }
+  else {
+    res.writeHead(404, {
+      "Content-Type": "text/html"
+    })
+    res.end(
+      "<h1>Page not found!</h1>"
+    )
   }
 
 });
